b5271056a6c0f8f09b98167d8eb6dc7d6e3d48dd
ishmeet5ingh <ishmeet3j@gmail.com>
2024-05-06 10:59:15 +0530

    updated

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -7,4 +7,22 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if(!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        console.log("file is uploaded on the cloudinary", response.url)
+
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+
+        return null
+    }
+}
+
 
