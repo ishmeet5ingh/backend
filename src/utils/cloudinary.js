57afb6fde65d0e1df9111a904a89fe33f4cec407
ishmeet5ingh <ishmeet3j@gmail.com>
2024-05-06 10:54:33 +0530

    updated

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,6 +1,10 @@
 import {vs as cloudinary} from 'cloudinary'
 import fs from 'fs'
 
-cloudinary.config({
-    
-})
+cloudinary.config({ 
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
