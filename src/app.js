b610ceb17d9ff295cc9679ce2f6d4bbbf34d028a
ishmeet5ingh <ishmeet3j@gmail.com>
2024-05-07 14:29:09 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -10,12 +10,20 @@ app.use(cors({
 }))
 
 app.use(express.json({limit: "16kb"}))
-
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
-
 app.use(express.static("public"))
-
 app.use(cookieParser())
- 
+
+// routes import 
+// files segregation
+import userRouter from './routes/user.routes.js'
+
+// routes declaration
+app.use("/api/v1/users", userRouter) // write middleware.
+// to get the router use middleware
+// when someuser write "/users" then control pass to `userRouter`.
+
+// http://localhost:8000/users/register
+                      // prefix
 
 export { app }
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,23 @@
+// Controllers are components responsible for handling incoming requests from clients, processing the data as necessary, and producing an appropriate response. 
+
+// they act as intermediaries between the routes defined in your applicaiton and the logic that needs to be executed to fulfill those routes.
+
+
+import {asyncHandler} from '../utils/asyncHandler.js'
+
+const registerUser = asyncHandler( async(req, res)=> {
+    res.status(200).json({
+        message: "OK"
+    })
+})
+
+export {registerUser}
+
+// next step is to create routes.
+
+/*
+   -> we have created a method.
+   -> there is one major work of node js.
+        -> method should run when a URL hits
+        -> for routes make a separete folder
+*/
