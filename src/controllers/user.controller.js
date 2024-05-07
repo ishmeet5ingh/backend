f114c1d9bbdd4274f216ca713752b1519b3a2f19
ishmeet5ingh <ishmeet3j@gmail.com>
2024-05-08 00:16:41 +0530

    updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,15 +3,67 @@
 // they act as intermediaries between the routes defined in your applicaiton and the logic that needs to be executed to fulfill those routes.
 
 
+import { ApiError } from '../utils/ApiError.js'
 import {asyncHandler} from '../utils/asyncHandler.js'
+import { User } from '../models/user.model.js'
+import { uploadOnCloudinary } from '../utils/cloudinary.js'
 
 const registerUser = asyncHandler( async(req, res)=> {
-    res.status(200).json({
-        message: "chai aur code"
+    // get user details from frontend.
+    // validation - not empty.
+    // check if user already exists.
+    // check for images, check for avatar.
+    // upload them to cloudinary, avatar.
+    // create user object - create entry in db
+    // remove password and refresh token field from response.
+    // check for user creation.
+    // return res.
+    const {fullname, email, username, password} = req.body // destructure.
+    console.log("email: ", email)
+
+    if(
+        [fullname, email, username, password].some((field)=> field?.trim() === "")
+    ){
+        throw new ApiError(400, "All fields are mendatory")
+    }
+
+    const existedUser = User.findOne({
+        $or: [{ username }, { email }] 
+    })// anyone
+
+    if(existedUser){
+        throw new ApiError(409, "user with email or username already exists")
+    }
+
+    const avatarLocalPath = req.files.avatar[0]?.path
+    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    // will get the path uploaded by multer.
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar file is required")
+    }
+   
+    const user = await User.create({
+        fullname,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        email,
+        password,
+        username: username.toLowerCase()
     })
+
 })
 
-export {registerUser}
+export {
+    registerUser
+}
 
 // next step is to create routes.
 
