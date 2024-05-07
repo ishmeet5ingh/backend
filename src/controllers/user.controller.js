3463af87c914088939109d7d340e81dbb56aecdd
ishmeet5ingh <ishmeet3j@gmail.com>
2024-05-08 00:17:23 +0530

    u

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler";
 
 const registerUser = asyncHandler( async(req, res)=> {
-    res.status(200).json({
-        message: "ok"
-    })
+    
 })
 
 export {registerUser}
